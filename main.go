22326a9385b5583c754a3c6e381ac68742b4c486
robin-vidal <robin.vidal.pro@gmail.com>
2026-04-27 00:00:24 +0200

    fix: stop instanciate VM if flag is empty

--- main.go
+++ main.go
@@ -12,16 +12,16 @@ func main() {
 	var romPathFlag = flag.String("romPath", "", "Path to the ROM you want to load")
 	flag.Parse()
 
-	emulator := vm.New()
-
 	if *romPathFlag == "" {
 		fmt.Fprintln(os.Stderr, "you must specify a ROM")
 		os.Exit(1)
 	}
 
+	emulator := vm.New()
+
 	err := emulator.LoadROM(*romPathFlag)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
